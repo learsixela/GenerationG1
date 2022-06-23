04c0630e723f8324a7f300ade51e577d572cb173
Israel Palma <israelpalma@MacBook-Pro-de-Israel.local>
2022-06-23 12:28:19 -0400

    Spring Data

--- src/main/java/com/generation/controllers/LicenciaController.java
+++ src/main/java/com/generation/controllers/LicenciaController.java
@@ -1,14 +1,20 @@
 package com.generation.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.generation.models.Licencia;
 
 @Controller
 @RequestMapping("/licencia")
 public class LicenciaController {
 
 	@RequestMapping("")
-	public String depliegue() {
+	public String depliegue(Model model) {
+		Licencia licencia = new Licencia();
+		//licencia.setEstado("activo");
+		model.addAttribute("licencia", licencia);
 		return "licencia.jsp";
 	}
 }
